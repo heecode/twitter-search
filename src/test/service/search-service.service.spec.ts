7ec3395f9fd69429caabc664e6a64590beccb9e1
heecode <iheemi@gmail.com>
2017-01-30 10:10:00 +0800

    update service test

--- src/test/service/search-service.service.spec.ts
+++ src/test/service/search-service.service.spec.ts
@@ -3,15 +3,42 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 import { SearchService } from '../../app/service/search-service.service';
 import { TwitterApiService } from '../../app/service/twitter-api.service';
+import {SearchFilter} from '../../app/model/search-filter';
+import {SearchInput} from '../../app/model/search-input';
 
 describe('SearchServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{provide:SearchService,useClass:TwitterApiService]
+      providers: [{provide:SearchService,useClass:TwitterApiService}]
     });
   });
 
   it('should ...', inject([SearchService], (service: SearchService) => {
     expect(service).toBeTruthy();
   }));
+
+describe('#search(item)', () => {
+  it('should not return anything if search item empty string', inject([SearchService], (service: SearchService) => {
+    let searchInput = new SearchInput({
+      item:''
+    }); 
+    var searchFilter = new SearchFilter();
+   expect(service.search(searchInput,searchFilter).searchResults).toEqual([]);
+  
+
+  }));
+
+   it('should  return anything if search item not empty string', inject([SearchService], (service: SearchService) => {
+    let searchInput = new SearchInput({
+      item:'heemi'
+    }); 
+    var searchFilter = new SearchFilter();
+  var results = service.search(searchInput,searchFilter);
+  var len = results.searchResults;
+   expect(len.length).toBeGreaterThan(0);
+
+  }));
+
+});
+
 });
